b5378a77c1bf0405151dedbb38c2bb8d2df7e627
Sandesh Pathak <sandeshpathak0112@gmail.com>
2016-11-25 13:36:27 -0500

    initiated test

---
+++ test/database/model.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var expect = require('chai').expect;
+var Room = require('../../server/db/models/room');
+var Score = require('../../server/db/models/score');
+var User = require('../../server/db/models/user');
+
+/**
+ *
+ * Start here!
+ *
+ * These tests describe the model that you'll be writing in models/article.js
+ *
+ */
+
