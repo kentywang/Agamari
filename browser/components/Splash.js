fe4e9d615d733d930a704a330bb126761c5fc027
Sandesh Pathak <sandeshpathak0112@gmail.com>
2016-11-30 15:45:57 -0500

    ok for demo

--- browser/components/Splash.js
+++ browser/components/Splash.js
@@ -30,13 +30,13 @@ class Splash extends Component {
                     left: "50%",
                     transform: "translate(-50%, -50%)"
                     }}/>
-                    <label htmlFor="name-box" data-error="Please enter nickname" data-success="right">Please enter nickname</label>
+                   {/* <label htmlFor="name-box" data-error="Please enter nickname" data-success="right">Please enter nickname</label>*/}
 
             <button className="Buttons" onClick={play} id="play-box">play</button>
           </div>
-          <div>
+         {/* <div>
             <button className="buttons" onClick={play} id="rules-box">Rules</button>
-          </div>
+          </div>*/}
          
         </div>
 
