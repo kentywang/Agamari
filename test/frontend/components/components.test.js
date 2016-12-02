a091216fb94c68e5e1f764551ccee1e6bb77f0e8
Sandesh Pathak <sandeshpathak0112@gmail.com>
2016-12-02 10:29:08 -0500

    merged working code from frontend branch

---
+++ test/frontend/components/components.test.js
@@ -0,0 +1,27 @@
+import React, { Component }  from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import {spy} from 'sinon';
+
+import ControlPanel from '../../../browser/components/ControlPanel';
+
+describe('ControlPanel component tests', () => {
+
+    let slider, onChangeSpy;
+    beforeEach('Create component and onChange spy', () => {
+        onChangeSpy = spy();
+        slider = shallow(<ColorSlider onChange={onChangeSpy} />);
+    });
+
+    it('has min 0 and max 255', () => {
+        const el = slider.get(0);
+        expect(el.props.min).to.be.equal('0');
+        expect(el.props.max).to.be.equal('255');
+    });
+
+    it('calls passed in onChange prop with value of change event', () => {
+        slider.simulate('change', { target: { value: 13 } });
+        expect(onChangeSpy.called).to.be.true;
+    });
+
+});
