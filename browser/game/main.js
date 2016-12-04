3aff7d765c415f6df93a61f2072fe4b293c2c17c
Kenty Wang <kentymail@gmail.com>
2016-12-04 13:57:43 -0500

    removed fps stats

--- browser/game/main.js
+++ browser/game/main.js
@@ -671,9 +671,9 @@ scene.add(camera)
 
   loadGame();
 
- stats = new Stats();
-stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild( stats.dom );
+//  stats = new Stats();
+// stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+// document.body.appendChild( stats.dom );
 
 
 
