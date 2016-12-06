157c96fb5e90a9bd17084ae82f8b4435afa807e0
Kenty Wang <kentymail@gmail.com>
2016-12-06 14:49:34 -0500

    more balancing

--- browser/game/game.js
+++ browser/game/game.js
@@ -41,7 +41,7 @@ export function loadEnvironment() {
 
       // add mass to self according to scale
       if (id === socket.id){
-        playerObject.cannon.mass = 40 + (scale * 4);
+        playerObject.cannon.mass = 30 + (scale * 6);
       }
 
       // scale name text
