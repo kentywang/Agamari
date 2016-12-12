209894c3e63e7059e25df57d08db5bd8423ab692
Tom Schuster <tomjschuster@gmail.com>
2016-12-12 03:26:18 -0500

    update dev db name to agamari

--- server/db/_db.js
+++ server/db/_db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const name = (process.env.DATABASE_NAME || "blobworld");
+const name = (process.env.DATABASE_NAME || 'agamari');
 
 const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`;
 
