71756b501fa1d1e9cf25ab56a7b43aa658558685
Kenty Wang <kentymail@gmail.com>
2017-10-06 19:18:30 -0400

    remove drop

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ require(path.join(__dirname, 'db')).db.sync()
 
 // keep server from falling asleep
 setInterval(function() {
-    require(path.join(__dirname, 'db')).db.drop(); // drop previous tables so we don't ever run out of usable rows
+    // require(path.join(__dirname, 'db')).db.drop(); // drop previous tables so we don't ever run out of usable rows
 
     http.get("http://agamari.herokuapp.com"); // fire ping
 }, 300000); // every 5 minutes (300000)
