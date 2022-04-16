81898a0c304e9c548f94f9a8ddd00a8e7f5e0e04
Kenty Wang <kwang@indigoag.com>
2022-04-16 10:18:13 -0400

    add compat for local and heroku deploys

--- server/db/_db.js
+++ server/db/_db.js
@@ -4,17 +4,21 @@ const name = (process.env.DATABASE_NAME || 'agamari');
 
 const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`;
 
-const db = new Sequelize(url, {
-  logging: false,
-  define: { underscored: true },
-  dialectOptions: {
-    ...(  // only add this  when on heroku, since local doesn't support SSL cxns
-      process.env.PORT ? {
+const db = process.env.PORT ? (
+  new Sequelize(url, {
+      logging: false,
+      define: { underscored: true },
+      dialectOptions: {
         ssl: {
           rejectUnauthorized: false
         }
-      } : {}
-    )
-  }
-});
+      }
+  })
+) : (
+  new Sequelize(url, {
+    logging: false,
+    define: { underscored: true },
+  })
+)
+
 module.exports = db;
