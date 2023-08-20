9d6121866dd9785966dc3e4812b2043b7ff0edf9
kentywang <kentymail@gmail.com>
2023-08-20 13:09:51 -0400

    rm cookie session

--- server/middleware.js
+++ server/middleware.js
@@ -2,11 +2,7 @@ const express = require('express'),
       path = require('path')
 
 const applyMiddleware = app => {
-  app.use(require('cookie-session') ({
-    name: 'session',
-    keys: [process.env.SESSION_SECRET || 'an insecure secret key'],
-  }))
-   .use(express.static(path.join(__dirname, '..', 'public')))
+  app.use(express.static(path.join(__dirname, '..', 'public')))
    .use('/materialize-css',
       express.static(path.join(__dirname, '..', 'node_modules', 'materialize-css', 'dist')))
 };
