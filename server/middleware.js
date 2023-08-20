88a54847f4e17270d2eae781f13aeb159d99e25d
kentywang <kentymail@gmail.com>
2023-08-20 13:02:39 -0400

    remove more packages

--- server/middleware.js
+++ server/middleware.js
@@ -1,19 +1,11 @@
 const express = require('express'),
-      bodyParser = require('body-parser'),
-      morgan = require('morgan'),
-      path = require('path'),
-      passport = require('passport');
+      path = require('path')
 
 const applyMiddleware = app => {
   app.use(require('cookie-session') ({
     name: 'session',
     keys: [process.env.SESSION_SECRET || 'an insecure secret key'],
   }))
-   .use(bodyParser.json())
-   .use(bodyParser.urlencoded({ extended: false }))
-   .use(morgan('dev'))
-   .use(passport.initialize())
-   .use(passport.session())
    .use(express.static(path.join(__dirname, '..', 'public')))
    .use('/materialize-css',
       express.static(path.join(__dirname, '..', 'node_modules', 'materialize-css', 'dist')))
